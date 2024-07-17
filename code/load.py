487ba643467c6eea6c243e9e0c464ac0ff37c6a6
Gugo-le <rngustmd0719@naver.com>
2024-07-17 19:50:17 +0900

    reset

--- code/load.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-# CSV 파일 로드
-coca_df = pd.read_csv('assets/files/COCA.csv')
-tokens_df = pd.read_csv('assets/files/COCA_tokens.csv')
-
-# 데이터 확인
-print(coca_df.head())
-print(tokens_df.head())
