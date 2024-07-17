e4450bea17dbb2943cf4015feb15338874e74ae2
Gugo-le <rngustmd0719@naver.com>
2024-07-17 20:38:48 +0900

    dic

--- lib/freq.py
+++ lib/freq.py
@@ -5,8 +5,11 @@ from collections import Counter
 coca_df = pd.read_csv('assets/files/COCA.csv')
 tokens_df = pd.read_csv('assets/files/COCA_tokens.csv')
 
-# 단어 빈도 계산
-word_freq = Counter(tokens_df['word'])
 
-# 상위 10개 단어 출력
-print(word_freq.most_common(10))
+# 단어와 빈도를 딕셔너리로 변환
+word_freq = dict(zip(tokens_df['Token'], tokens_df['Count']))
+
+top_words = Counter(word_freq).most_common(10)
+print("\n빈도 높은 단어 상위 10개:")
+for word, freq in top_words:
+    print(f"{word}: {freq}")
