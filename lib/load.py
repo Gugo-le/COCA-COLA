2e95cc66b4880efa26c0af2fc12522fe0fea68c8
Gugo-le <rngustmd0719@naver.com>
2024-07-17 20:10:09 +0900

    file path error fixed

--- lib/load.py
+++ lib/load.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 # CSV 파일 로드
 coca_df = pd.read_csv('assets/files/COCA.csv')
-tokens_df = pd.read_csv('ssets/files/COCA_token.csv')
+tokens_df = pd.read_csv('assets/files/COCA_tokens.csv')
 
 # 데이터 확인
 print(coca_df.head())
